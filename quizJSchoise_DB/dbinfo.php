1a459a8e60eddf1ead37ffbc30b3389b904e819b
AYA-kurokiri09 <ice6e63@gmail.com>
2020-12-06 20:30:31 +0900

    css適用

--- quizJSchoise_DB/dbinfo.php
+++ quizJSchoise_DB/dbinfo.php
@@ -1,6 +1,6 @@
 <?php
   //ローカル:3〜16行目を使用　本番:18〜25行目を使用
-  /* define('DB_HOST', 'mysql');
+  define('DB_HOST', 'mysql');
   define('DB_USER', 'default');
   define('DB_PASSWORD', 'root');
   define('DB_NAME', 'mysql');
@@ -13,8 +13,8 @@
     print('接続エラーが発生しました。:'.$e->getMessage());
     exit;
   } 
-*/
 
+/*
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $db_host = $url["host"];
 $user = $url["user"];
@@ -22,5 +22,5 @@ $password = $url["pass"];
 $db_name = substr($url["path"], 1);
 
 $dsn = 'mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8mb4;';
-$pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+$pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));*/
 ?>
